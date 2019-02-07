a8459beae4e5b802069a6a8338ff439d8a0b2833
podonoghue <podonoghue@swin.edu.au>
2019-02-07 23:27:39 +1100

    V4.12.1.230 - Corrected S32KEVB firmware choice

--- USBDM_Kinetis/Collect.cmd
+++ USBDM_Kinetis/Collect.cmd
@@ -33,23 +33,28 @@ for %%f in (%FIRMWARE_VERSIONS%) do copy "%CW_Kinetis_ROOT%\%%f\*.sx"        "%M
 set FIRMWARE_VERSIONS=OpenSDAv2_0 OpenSDAv2_0_Power OpenSDAv2_0_Unique_ID OpenSDAv2_0_Unique_ID_Power OpenSDAv2_1_Power OpenSDAv2_1_Unique_ID_Power
 for %%f in (%FIRMWARE_VERSIONS%) do copy /B "%CW_Kinetis_ROOT%\%%f\*.bin"        "%MK_FLASH_REFERENCE%"
 
+REM OpenSDA V1 (P&E)
 set BOARD=FRDM-KE02Z FRDM-KE02Z40M FRDM-KE04Z FRDM-KE06Z FRDM-KL02Z FRDM-KL03Z FRDM-KL05Z FRDM-KL25Z FRDM-KL25Z
-set BOARD=%BOARD% FRDM-KL26Z, FRDM-KL27Z, FRDM-KL43Z, FRDM-KL46Z, FRDM-K20D50M, S32K144EVM
+set BOARD=%BOARD% FRDM-KL26Z FRDM-KL27Z FRDM-KL43Z FRDM-KL46Z FRDM-K20D50M
 for %%f in (%BOARD%) do copy "%CW_Kinetis_ROOT%\OpenSDAv1\*.sx"            %MK_FLASH_DIR%\%%f.sx
 for %%f in (%BOARD%) do copy "%CW_Kinetis_ROOT%\OpenSDAv1_Unique_ID\*.sx"  %MK_FLASH_DIR%\%%f_Unique_ID.sx
 
-set BOARD=FRDM-KEAZ64Q64 FRDM-KEAZN32Q64 FRDM-KEAZ128Q80  
+REM OpenSDA V1 + Power (P&E)
+set BOARD=FRDM-KEAZ64Q64 FRDM-KEAZN32Q64 FRDM-KEAZ128Q80 S32K144EVM
 for %%f in (%BOARD%) do copy "%CW_Kinetis_ROOT%\OpenSDAv1_Power\*.sx"            %MK_FLASH_DIR%\%%f.sx
 for %%f in (%BOARD%) do copy "%CW_Kinetis_ROOT%\OpenSDAv1_Unique_ID_Power\*.sx"  %MK_FLASH_DIR%\%%f_Unique_ID.sx
 
+REM OpenSDA V2
 set BOARD=FRDM-KL82Z
 for %%f in (%BOARD%) do copy /B "%CW_Kinetis_ROOT%\OpenSDAv2_0\*.bin"            %MK_FLASH_DIR%\%%f.bin
 for %%f in (%BOARD%) do copy /B "%CW_Kinetis_ROOT%\OpenSDAv2_0_Unique_ID\*.bin"  %MK_FLASH_DIR%\%%f_Unique_ID.bin
 
+REM OpenSDA V2 + Power
 set BOARD=FRDM-K64F
 for %%f in (%BOARD%) do copy /B "%CW_Kinetis_ROOT%\OpenSDAv2_0_Power\*.bin"            %MK_FLASH_DIR%\%%f.bin
 for %%f in (%BOARD%) do copy /B "%CW_Kinetis_ROOT%\OpenSDAv2_0_Unique_ID_Power\*.bin"  %MK_FLASH_DIR%\%%f_Unique_ID.bin
 
+REM OpenSDA V2.1 + Power
 set BOARD=FRDM-K22F FRDM-K28F FRDM-K66F FRDM-K82F FRDM-KV10Z FRDM-KV31F FRDM-KE15Z 
 for %%f in (%BOARD%) do copy /B "%CW_Kinetis_ROOT%\OpenSDAv2_1_Power\*.bin"            %MK_FLASH_DIR%\%%f.bin
 for %%f in (%BOARD%) do copy /B "%CW_Kinetis_ROOT%\OpenSDAv2_1_Unique_ID_Power\*.bin"  %MK_FLASH_DIR%\%%f_Unique_ID.bin
