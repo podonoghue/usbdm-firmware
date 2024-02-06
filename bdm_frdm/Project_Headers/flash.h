e6a854e3c3adfc836f65a1f2e723a04c34f9efbe
Peter O'Donoghue <1848579+podonoghue@users.noreply.github.com>
2024-02-06 17:10:47 +1100

    V4.12.1.325 - Cleanup

--- bdm_frdm/Project_Headers/flash.h
+++
@@ -1,8 +0,0 @@
-/**
- * @file    flash.h (180.ARM_Peripherals/Project_Headers/flash.h)
- * @brief   Flash support
- *
- *  Created on: 21 Sep 2016
- *      Author: podonoghue
- */
-#include "ftfl.h"
